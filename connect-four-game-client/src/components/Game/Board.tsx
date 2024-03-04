63341b07989b5591555c0cc4649242e04b1671bd
chonhenry <chonhenry@gmail.com>
2024-03-04 12:36:16 -0500

    board

--- connect-four-game-client/src/components/Game/Board.tsx
+++ connect-four-game-client/src/components/Game/Board.tsx
@@ -5,9 +5,29 @@ import redTurn from "../../assets/images/turn-background-red.svg";
 import yellowTurn from "../../assets/images/turn-background-yellow.svg";
 import Circle from "./Circle";
 
+interface Position {
+  row: number;
+  col: number;
+}
+
+const ROW_COUNT = 6;
+const COL_COUNT = 7;
+
 const Board = () => {
   const left = [19, 102, 186, 270, 353, 436, 520];
   const top = [19, 102, 186, 269, 352, 436];
+
+  function generateGrid(): Position[][] {
+    const grid = [];
+    for (let i = 0; i < ROW_COUNT; i++) {
+      const row = [];
+      for (let j = 0; j < COL_COUNT; j++) {
+        row.push({ row: i, col: j });
+      }
+      grid.push(row);
+    }
+    return grid;
+  }
 
   const generateSingleColumn = (left: number, col_idx: number) => {
     return (
@@ -32,10 +52,58 @@ const Board = () => {
     );
   };
 
+  function generateBoard() {
+    return top.map((top, row_idx) => {
+      return (
+        <div
+          key={`row-${row_idx}`}
+          id={`row-${row_idx}`}
+          className={`bg-re absolute h-[61px] w-full top-[${top}px]`}
+        >
+          <div className="relative h-full w-full">
+            {generateRow(row_idx, top)}
+          </div>
+        </div>
+      );
+    });
+  }
+
+  function generateRow(row_idx: number, top: number) {
+    console.log(row_idx, top);
+    return left.map((left, col_idx) => (
+      <Circle
+        key={`row-${row_idx}-col-${col_idx}`}
+        left={left}
+        col_idx={col_idx}
+        row_idx={row_idx}
+      />
+    ));
+  }
+
   return (
     <div className="w-full relative">
+      {/* <div className="bg-red absolute h-[61px] w-full top-[19px]">
+        <div className="relative h-full w-full">
+          <div className="bg-yellow absolute w-[61px] h-[61px] rounded-full left-[19px]"></div>
+        </div>
+      </div>
+      <div className="bg-red absolute h-[61px] w-full top-[102px]">
+        <div className="relative h-full w-full">
+          <div className="bg-yellow absolute w-[61px] h-[61px] rounded-full left-[102px]"></div>
+        </div>
+      </div>
+      <div className="bg-red absolute h-[61px] w-full top-[186px]"></div>
+      <div className="bg-red absolute h-[61px] w-full top-[269px]"></div>
+      <div className="bg-red absolute h-[61px] w-full top-[352px]"></div>
+      <div className="bg-red absolute h-[61px] w-full top-[436px]">
+        <div className="relative h-full w-full">
+          <div className="bg-yellow absolute w-[61px] h-[61px] rounded-full left-[520px]"></div>
+        </div>
+      </div> */}
+
       <img src={boardLayerWhite} className="absolute" />
-      {left.map((left, idx) => generateSingleColumn(left, idx))}
+      {generateBoard()}
+      {/* {left.map((left, idx) => generateSingleColumn(left, idx))} */}
       <img src={boardLayerBlack} className="" />
 
       <div className="bg-yello absolute w-full bottom-0 translate-y-[35px] flex justify-center">
--- connect-four-game-client/src/components/Game/Circle.tsx
+++ connect-four-game-client/src/components/Game/Circle.tsx
@@ -1,20 +1,19 @@
 import { FC, useState } from "react";
 
 interface CircleProps {
-  top: number;
+  top?: number;
+  left?: number;
   col_idx: number;
   row_idx: number;
 }
 
-const Circle: FC<CircleProps> = ({ top, col_idx, row_idx }) => {
-  const mapping = [5, 4, 3, 2, 1, 0];
-
+const Circle: FC<CircleProps> = ({ top, left, col_idx, row_idx }) => {
   const [isSelected, setIsSelected] = useState(false);
 
   function position(): string {
     const pos = {
+      row: row_idx,
       col: col_idx,
-      row: mapping[row_idx],
     };
 
     return JSON.stringify(pos);
@@ -23,25 +22,42 @@ const Circle: FC<CircleProps> = ({ top, col_idx, row_idx }) => {
   function getPosition(event: React.MouseEvent<HTMLDivElement>): void {
     const position = JSON.parse(event.currentTarget.id);
 
-    // console.log(position);
+    console.log(position);
   }
 
   return isSelected ? (
     <div
       id={position()}
-      className={`bg-yellow absolute w-[61px] h-[61px] rounded-full top-[${top}px] cursor-pointer`}
+      className={`bg-yellow absolute w-[61px] h-[61px] rounded-full left-[${left}px]`}
       onClick={getPosition}
     ></div>
   ) : (
     <div
       id={position()}
-      className={`hover:bg-yellow absolute w-[61px] h-[61px] rounded-full top-[${top}px] cursor-pointer`}
+      className={`hover:bg-yellow absolute w-[61px] h-[61px] rounded-full left-[${left}px] cursor-pointer`}
       onClick={(e) => {
         setIsSelected(true);
         getPosition(e);
       }}
     ></div>
   );
+
+  // return isSelected ? (
+  //   <div
+  //     id={position()}
+  //     className={`bg-yellow absolute w-[61px] h-[61px] rounded-full top-[${top}px] cursor-pointer`}
+  //     onClick={getPosition}
+  //   ></div>
+  // ) : (
+  //   <div
+  //     id={position()}
+  //     className={`hover:bg-yellow absolute w-[61px] h-[61px] rounded-full top-[${top}px] cursor-pointer`}
+  //     onClick={(e) => {
+  //       setIsSelected(true);
+  //       getPosition(e);
+  //     }}
+  //   ></div>
+  // );
 };
 
 export default Circle;
